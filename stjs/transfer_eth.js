f93e6579c0744f6603cbd18531752ee10c37ebc9
Luciano Augusto Campagnoli da Silva <luti4497@gmail.com>
2024-11-12 18:12:48 -0300

    feature: Ajusta script para funding massivo

--- stjs/transfer_eth.js
+++ stjs/transfer_eth.js
@@ -1,32 +1,69 @@
+const { Account, RpcProvider, Contract } = require("starknet");
+const fs = require("fs");
+const { accounts } = require("./accounts.json");
+
+const transferFunds = async ({
+    provider,
+    ethContract,
+    starkContract,
+    address
+}) => {
+
+    await provider.waitForTransaction(
+        (await ethContract.transfer(address, 100)).transaction_hash
+    );
+    console.log("Transferiu o ETH para o endereço: ", address);
+
+    await provider.waitForTransaction(
+        (await starkContract.transfer(address, 100)).transaction_hash
+    );
+    console.log("Transferiu o STRK para o endereço: ", address);
+
+    console.log("Transferência realizada com sucesso!");
+
+    const balanceStark = await starkContract.balanceOf(address);
+    const balanceEth = await ethContract.balanceOf(address);
+
+    console.log(`Saldo ETH da conta ${address}: ${balanceEth}`);
+    console.log(`Saldo STRK da conta ${address}: ${balanceStark}`);
+
+    return {
+        address,
+        balanceStark,
+        balanceEth
+    }
+}
+
+
 (async () => {
-    const { Account, RpcProvider, Contract } = require("starknet");
+
+    const ethContractAddress = "0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7";
+    const starkContractAddress = "0x04718f5a0fc34cc1af16a1cdee98ffb20c31f5cd61d6ab07201858f4287c938d";
 
     // initialize provider
     const provider = new RpcProvider({ nodeUrl: 'https://starknet-sepolia.infura.io/v3/1bc8c51edac6463eaebc92a80761f076' });
     // initialize existing pre-deployed account 0 of Devnet
 
-    const address = "0x07932bfde40a0aa7ece16a2972bd6b4df86b864916d4b5f4fbf2f68aa334ef74";
+    const sender = new Account(provider, "0x06c8fc58c3d7a09760ac090b6ceebf86174e5537e0cd949ec53de5f45bc29d1f", "0x02a9e9f77a29ac80ecd2bac48e5b7865f3c4012a4717559a7c36aabe5d192f80");
 
-    const account = new Account(provider, address, "0x074a5773cc5fe14b48001a4a0249ed45420dc21cd89b072e69079d62456ce04e");
+    const ethContract = new Contract((await provider.getClassAt(ethContractAddress)).abi, ethContractAddress, provider);
+    const starkContract = new Contract((await provider.getClassAt(starkContractAddress)).abi, starkContractAddress, provider);
 
-    const compressedContract = await provider.getClassAt("0x04718f5a0fc34cc1af16a1cdee98ffb20c31f5cd61d6ab07201858f4287c938d");
+    ethContract.connect(sender);
+    starkContract.connect(sender);
+    const balances = [];
 
-    const ethContract = new Contract(compressedContract.abi, "0x04718f5a0fc34cc1af16a1cdee98ffb20c31f5cd61d6ab07201858f4287c938d", provider);
+    for (let index = 0; index < accounts.length; index++) {
+        balances.push(
+            await transferFunds({
+                provider,
+                ethContract,
+                starkContract,
+                address: accounts[index].address
+            })
+        )
+    }
 
-    ethContract.connect(account);
-
-    //const transferTx = await ethContract.transfer("0x3df225744da0722de4f13fd35546229b33f2f6776d31e51c0c13cc8144e60a6", 100);
-    console.log("Transferência realizada com sucesso!");
-    const balanceInitial = await ethContract.balanceOf("0x3df225744da0722de4f13fd35546229b33f2f6776d31e51c0c13cc8144e60a6");
-    console.log("Saldo: ", balanceInitial)
-
-    // const result = await voteContract.get_vote_status();
-    // console.log("Result:");
-    // console.log(result)
-
-    // const isRegistered = await voteContract.voter_can_vote("0x07932bfde40a0aa7ece16a2972bd6b4df86b864916d4b5f4fbf2f68aa334ef74");
-
-    // console.log("Is registered? ", isRegistered)
-
-    
+    console.log("Saldos: ");
+    console.log(balances);
 })()
